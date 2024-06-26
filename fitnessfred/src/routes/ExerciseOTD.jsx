170d6e7f27c233f50ab6977765a7696d200d4dfb
matthew <shaggysci3@gmail.com>
2024-06-26 16:31:24 -0400

    clean up eotd button

--- fitnessfred/src/routes/ExerciseOTD.jsx
+++ fitnessfred/src/routes/ExerciseOTD.jsx
@@ -24,7 +24,7 @@ const ExerciseOTD = () =>{
             <img src="https://cdn.pixilart.com/photos/large/e66f1f729001a88.png"></img>
             <div className="buttonContainer">
             <h2>{sets>0?`Sets Left: ${sets}`:"Great Job!"}</h2>
-            <h2 style={{margin:"15px",height:"9px"}} className="startButton" onClick={handleClick}>complete set</h2>
+            <h2 style={sets>0?{margin:"15px",height:"9px",}:{visibility:"hidden",width:"0",padding:"0"}} className="startButton" onClick={handleClick}>complete set</h2>
             </div>
             <h2 style={{margin:"10px"}} className="startButton" onClick={handleReset}>reset Sets</h2>
             {/* <h3>timer</h3> */}
